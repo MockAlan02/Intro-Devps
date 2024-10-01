2edeef235ae79fda19dfe7cb3680784a39a5bee8
MockAlan02 <alantubert09@gmail.com>
2024-09-30 22:09:17 -0400

    commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 function HelloWorld() {
   console.log("Hello World");
 }
 
-
 HelloWorld();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
